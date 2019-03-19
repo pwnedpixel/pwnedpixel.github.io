d5938691bf6c1e266df4777ca0cc159aee6f2475
Andy K <andy@akeech.com>
2019-03-19 14:38:55 -0400

    added deploy script for github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 
 @NgModule({
    imports: [
-       RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes, {useHash: false})
       ] ,
     exports: [RouterModule]
 })
